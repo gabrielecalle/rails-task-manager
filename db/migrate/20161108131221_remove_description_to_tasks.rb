7968c812fe6fa793c47875d66c7365c09c3f394e
gabriel ecalle <gabriel.ecalle@googlemail.com>
2016-11-08 14:15:00 +0100

    remove description column

---
+++ db/migrate/20161108131221_remove_description_to_tasks.rb
@@ -0,0 +1,5 @@
+class RemoveDescriptionToTasks < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :tasks, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108105052) do
+ActiveRecord::Schema.define(version: 20161108131221) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
-    t.string   "description"
     t.boolean  "done"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
